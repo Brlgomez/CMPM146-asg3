6441d9e46d74a3cc66256b11cebb4247e58c3220
Bryan Ambriz <bambriz@ucsc.edu>
2015-04-19 20:32:17 -0700

    update :0

--- p3_pathfinder.py
+++ p3_pathfinder.py
@@ -1,37 +1,37 @@
 from heapq import heappush, heappop
 def find_path(src, dest, mesh):
-    print mesh
     v = src
+    dF = dest
     q = []
     visited = []
     d = {}
     p = {}
-    d[src] = True
-    heappush(q,(d[src],src))
-    visited.append(src)
+    for z in mesh:
+        if (z[0] <= src[0] and z[2] >= src[0] and z[1]<= src[1] and z[3] >= src[1]):
+            v = z
+            visited.append(v)
+            d[v]= True
+        if(z[0] <= dest[0] and z[2] >= dest[0] and z[1] <= dest[1] and z[3] >= dest[1]):
+            dF = z
+
+   # print mesh
+
+    heappush(q,(d[v],v))
+    #visited.append(src)
     while q:
         dV, v = heappop(q)
-        if v is dest:
+        if v is dF:
             break
         neighbors = mesh['adj'].get(v,[])
         for w in neighbors:
-            if w is dest:
 
-             if d[w] is not True:
+            if d[w] is not True:
                 visited.append(w)
                 p[w] = v
                 d[w] = True
                 heappush(q, (d[w],w))
-    if v is dest:
-        path = []
-        node = dest
-        while node in p:
-            path.append(node )
-            node = p[node]
-        path.append(src)
-        path.reverse()
-        return (path, visited)
-    else:
-        return []
+
+    return ([(src, dest)], visited)
+
 
 
